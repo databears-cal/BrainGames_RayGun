dc3533797888c79b4ec460126da250b75a16d324
databears-cal <45097648+databears-cal@users.noreply.github.com>
2019-04-20 20:10:31 -0700

    Add files via upload

---
+++ ControllerScript_Kevin.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript_Kevin : MonoBehaviour
+{
+    public GameObject object_hit;
+    public GameObject temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = new GameObject();
+        object_hit = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider != null)
+            {
+                if (object_hit != hit.collider.gameObject)
+                {
+                    object_hit.transform.SendMessage("OnVRExit");
+                    object_hit = hit.transform.gameObject;
+                    Debug.Log("On VR Enter");
+                    object_hit.transform.SendMessage("OnVREnter") ;
+                }
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+                {
+                    object_hit.transform.SendMessage("OnVRTriggerDown");
+                }
+            }
+        } else
+        {
+            if (object_hit != null)
+            {
+                object_hit.transform.SendMessage("OnVRExit");
+                object_hit = temp;
+            }
+        }
+    }
+}
---
+++ InstructionPopUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionPopUp : MonoBehaviour
+{
+    public Text help;
+    public Text instructionText;
+
+    public string help_msg;
+    public string instruction_msg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        help = GameObject.Find("HelpText").GetComponent<Text>();
+        instructionText = GameObject.Find("InstructionText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnVREnter()
+    {
+        help.text = help_msg;
+        instructionText.text = instruction_msg;
+    }
+
+    void OnVRExit()
+    {
+        help.text = "";
+        instructionText.text = "";
+
+    }
+}
